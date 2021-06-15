7139e6e6c46cae1c64178d17154b0c6586977c4c
Irlan <irlansh@hotmail.com>
2021-06-15 12:55:29 -0300

    Criadas as funcionalidades de soma, subtração, multiplicação, divisão

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,9 +4,8 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
-		double valor1;
-		double valor2;
-		double soma;
+		double valor1, valor2;
+		double soma, divisao, multiplicacao, subtracao;
 		System.out.println("Por favor digite um valor");
 		valor1 = leitor.nextDouble();
 		
@@ -14,8 +13,14 @@ public class Calculadora {
 		valor2 = leitor.nextDouble();
 		
 		soma = valor1 + valor2;
+		subtracao = valor1 - valor2;
+		multiplicacao = valor1 * valor2;
+		divisao = valor1 / valor2;
 		
 		System.out.println("A soma dos 2 valores informados é,  " + soma );
+		System.out.println("A subtração dos 2 valores informados é,  " + subtracao );
+		System.out.println("A multiplicação dos 2 valores informados é,  " + multiplicacao );
+		System.out.println("A divisão dos 2 valores informados é,  " + divisao );
 		leitor.close();
 	}
 
